c167c09f9dd07a1131d77d283ef3fc798f93495c
gusjak <70512255+gusjak@users.noreply.github.com>
2020-12-04 14:17:24 +0100

    Click counter
    Added the pop-cat meme and a click counter with a sound effect when clicking.

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,25 @@
-setTimeout(function () {
-  document.getElementById('party').play();
-  console.log('Party mode initiated!');
-}, 1500);
+const img = document.querySelector('img');
+const popSound = new Audio();
+popSound.src = '/public/sfx/pop.ogg';
+popSound.preload = 'auto';
+
+img.addEventListener('click', function () {
+  if (this.classList.contains('changed')) {
+    this.src = '/public/img/closed.png';
+    this.className = '';
+  } else {
+    this.className = 'changed';
+    this.src = '/public/img/opened.png';
+  }
+  popSound.play();
+});
+
+const clickCounter = document.querySelector('h2');
+
+let counter = document.querySelector('.imgBtn'),
+  count = 0;
+
+counter.addEventListener('click', function () {
+  count += 1;
+  clickCounter.innerHTML = 'You have clicked me' + '  ' + count + '  ' + 'times!';
+});
